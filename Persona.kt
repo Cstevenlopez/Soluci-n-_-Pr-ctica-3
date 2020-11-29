3c8b55adda73d013dd39e7b71900a41a63d6ae38
Cstevenlopez <cstevenlopez2001@gmail.com>
2020-11-29 08:31:58 -0600

    Ejemplo persona.kt

---
+++ Persona.kt
@@ -0,0 +1,31 @@
+/*class Persona{
+    var nombre:String = ""
+    var apellido:String = ""
+    constructor(){
+    }
+    constructor(n:String, a:String):this()
+    {
+    nombre=n
+    apellido=a
+    }
+   }
+   fun main(){
+    var persona = Persona("Yesser","Miranda")
+    var nueva = Persona()
+    nueva.nombre = "Juan"
+    nueva.apellido = "Lopez"
+    println(persona.nombre)
+    println(nueva.nombre)
+   }
+   */
+ class Persona(var nombre:String, var apellido:String)
+
+fun main(){
+ var persona = Persona("Yesser","Miranda")
+ /*var nueva = Persona()
+ nueva.nombre = "Juan"
+ nueva.apellido = "Lopez"
+ */
+ println(persona.apellido)
+ /*println(nueva.nombre)*/
+}
